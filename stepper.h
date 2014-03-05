9460779d6f25316de690b08119fef5bf407c0159
Stefan Bossbaly <sbossb@gmail.com>
2014-03-05 17:49:01 -0500

    Added stepper header file
    This is a basic header file of the stepper that we will be using.
    I am going to add more functions later but this will do for now.

---
+++ stepper.h
@@ -0,0 +1,30 @@
+#ifndef STEPPER_H_
+#define STEPPER_H_
+
+#include "inttypes.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define FORWARD 0
+#define REVERSE 1
+
+typedef struct
+{
+	uint8_t step;
+	uint8_t pin0, pin1, pin2, pin3;
+} stepper_t;
+
+void stepper_init(stepper_t *stepper, uint8_t pin0, uint8_t pin1, uint8_t pin2, uint8_t pin3);
+void stepper_step(stepper_t *stepper, uint8_t direction);
+void stepper_release(stepper_t *stepper);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* STEPPER_H_ */
