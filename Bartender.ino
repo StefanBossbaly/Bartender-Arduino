ecee00b6ca19dd1b9018c261d7006caac7b59e77
Stefan Bossbaly <sbossb@gmail.com>
2014-03-05 19:34:00 -0500

    Use stepper.h
    Replace the test code with the code that is now in the stepper
    functions.

--- Bartender.ino
+++ Bartender.ino
@@ -1,42 +1,13 @@
-int motorPin1 = 2;
-int motorPin2 = 3;
-int motorPin3 = 4;
-int motorPin4 = 5;
-int delayTime = 3;
+#include "stepper.h"
 
+stepper_t stepper;
 
 void setup()
 {
-	pinMode(motorPin1, OUTPUT);
-	pinMode(motorPin2, OUTPUT);
-	pinMode(motorPin3, OUTPUT);
-	pinMode(motorPin4, OUTPUT);
+	stepper_init(&stepper, 2, 3, 4, 5);
 }
 
 void loop()
 {
-	digitalWrite(motorPin1, HIGH);
-	digitalWrite(motorPin2, LOW);
-	digitalWrite(motorPin3, HIGH);
-	digitalWrite(motorPin4, LOW);
-	delay(delayTime);
-	
-	digitalWrite(motorPin1, LOW);
-	digitalWrite(motorPin2, HIGH);
-	digitalWrite(motorPin3, HIGH);
-	digitalWrite(motorPin4, LOW);
-	delay(delayTime);
-	
-	digitalWrite(motorPin1, LOW);
-	digitalWrite(motorPin2, HIGH);
-	digitalWrite(motorPin3, LOW);
-	digitalWrite(motorPin4, HIGH);
-	delay(delayTime);
-	
-	digitalWrite(motorPin1, HIGH);
-	digitalWrite(motorPin2, LOW);
-	digitalWrite(motorPin3, LOW);
-	digitalWrite(motorPin4, HIGH);
-	delay(delayTime);
-	
+	stepper_step(&stepper, REVERSE);
 }
