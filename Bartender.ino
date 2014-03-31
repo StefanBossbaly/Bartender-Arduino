73e4ef5ad7c55f781f714c332ec5b56ec1799eec
Stefan Bossbaly <sbossb@gmail.com>
2014-03-31 17:36:26 -0400

    Added protocol test

--- Bartender.ino
+++ Bartender.ino
@@ -2,24 +2,35 @@
 #include "protocol.h"
 #include "inttypes.h"
 #include "serial.h"
+#include "handler.h"
 
 stepper_t stepper;
+handler_t handler;
+uint8_t command[MSG_SIZE];
+uint8_t response[MSG_SIZE];
+int size = 0;
 
 void setup()
 {
 	serial_begin(9600);
+	handler_init(&handler, &stepper);
 }
 
 
 void loop()
-{	
+{
+	if (serial_available() > 0)
+	{
+		serial_read_byte(&command[size]);
+		size++;
+	}
 	
-	char data[] = "ABC";
+	if (size == 32)
+	{
+		handler_handle(&handler, command, response);
+		serial_write_chunk(response, MSG_SIZE);
+		size = 0;
+	}
 	
-	serial_write_chunk(data, 3);
-	//serial_write_byte(0x041);
-	//serial_write_byte(0x0D);
-	
-	
-	delay(1000);
+	delay(10);
 }
