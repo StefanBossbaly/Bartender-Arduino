0a20408a5948995e67a4871a9807fd9170b44571
Stefan Bossbaly <sbossb@gmail.com>
2014-03-31 17:38:28 -0400

    Added error codes to the serial class
    In case that and error occurs we need to be able to communicate
    it with the calling function. To do this the easiest way is to
    return an error code and if we need to return a value simply pass
    in a pointer.

--- serial.c
+++ serial.c
@@ -23,49 +23,69 @@ typedef struct {
 tx_buffer tx_buff;
 rx_buffer rx_buff;
 
-static void tx_store_byte(uint8_t byte) {
+static uint8_t tx_store_byte(uint8_t byte)
+{
 	uint8_t head = (uint8_t) ((tx_buff.head + 1) % USART_TX_BUFFER_SIZE);
 
 	// Make sure that we have space
-	if (head != tx_buff.tail) {
+	if (head != tx_buff.tail)
+	{
 		tx_buff.buffer[tx_buff.head] = byte;
 		tx_buff.head = head;
+
+		return E_NO_ERROR;
 	}
+
+	return E_BUFF_OVERFLOW;
 }
 
-static uint8_t tx_read_byte() {
-	if (tx_buff.head != tx_buff.tail) {
+static uint8_t tx_read_byte(uint8_t *data_out)
+{
+	if (tx_buff.head != tx_buff.tail)
+	{
 		uint8_t data = tx_buff.buffer[tx_buff.tail];
 		tx_buff.tail = (uint8_t) ((tx_buff.tail + 1) % USART_TX_BUFFER_SIZE);
-		return data;
+
+		*data_out = data;
+
+		return E_NO_ERROR;
 	}
 
-	// TODO set error code
-	return -1;
+	return E_EMPTY;
 }
 
-static void rx_store_byte(uint8_t byte) {
+static uint8_t rx_store_byte(uint8_t byte)
+{
 	uint8_t head = (uint8_t) ((rx_buff.head + 1) % USART_RX_BUFFER_SIZE);
 
 	// Make sure that we have space
-	if (head != rx_buff.tail) {
+	if (head != rx_buff.tail)
+	{
 		rx_buff.buffer[rx_buff.head] = byte;
 		rx_buff.head = head;
+
+		return E_NO_ERROR;
 	}
+
+	return E_BUFF_OVERFLOW;
 }
 
-static uint8_t rx_read_byte() {
-	if (rx_buff.head != rx_buff.tail) {
+static uint8_t rx_read_byte(uint8_t *data_out)
+{
+	if (rx_buff.head != rx_buff.tail)
+	{
 		uint8_t data = rx_buff.buffer[rx_buff.tail];
 		rx_buff.tail = (uint8_t) ((rx_buff.tail + 1) % USART_RX_BUFFER_SIZE);
-		return data;
+
+		*data_out = data;
+		return E_NO_ERROR;
 	}
 
-	// TODO set error code
-	return -1;
+	return E_EMPTY;
 }
 
-void serial_begin(unsigned long baud) {
+void serial_begin(unsigned long baud)
+{
 	rx_buff.head = rx_buff.tail = 0;
 	tx_buff.head = tx_buff.tail = 0;
 
@@ -102,15 +122,18 @@ void serial_begin(unsigned long baud) {
 	UCSR0B |= (1 << RXCIE0);
 }
 
-uint8_t serial_read_byte() {
-	uint8_t data;
+uint8_t serial_read_byte(uint8_t *data_out)
+{
+	uint8_t data, status;
 
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
 	{
-		data = rx_read_byte();
+		status = rx_read_byte(&data);
 	}
 
-	return data;
+	*data_out = data;
+
+	return status;
 }
 
 uint8_t serial_available()
@@ -118,26 +141,36 @@ uint8_t serial_available()
 	return (uint8_t) (USART_RX_BUFFER_SIZE + rx_buff.head - rx_buff.tail) % USART_RX_BUFFER_SIZE;
 }
 
-void serial_write_byte(uint8_t data)
+uint8_t serial_write_byte(uint8_t data)
 {
+	uint8_t status;
+
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
 	{
 		// Store the byte in our buffer
-		tx_store_byte(data);
+		status = tx_store_byte(data);
 
 		// Enable Data Register Empty interrupt
 		UCSR0B |= (1 << UDRIE0);
 	}
+
+	return status;
 }
 
-void serial_write_chunk(void *data, uint8_t size)
+uint8_t serial_write_chunk(void *data, uint8_t size)
 {
+	uint8_t status;
 	uint8_t *ptr = (uint8_t *) data;
 
 	for (uint8_t i = 0; i < size; i++)
 	{
-		serial_write_byte(*(ptr + i));
+		if ((status = serial_write_byte(*(ptr + i))) != E_NO_ERROR)
+		{
+			return status;
+		}
 	}
+
+	return E_NO_ERROR;
 }
 
 /**
@@ -165,6 +198,9 @@ ISR (USART_UDRE_vect)
 	// Put the next byte into the tx register
 	else
 	{
-		UDR0 = tx_read_byte();
+		uint8_t data;
+		tx_read_byte(&data);
+		UDR0 = data;
+
 	}
 }
--- serial.h
+++ serial.h
@@ -16,14 +16,19 @@ extern "C"
 #include <inttypes.h>
 
 
-#define USART_RX_BUFFER_SIZE 16
-#define USART_TX_BUFFER_SIZE 32
+#define USART_RX_BUFFER_SIZE 64
+#define USART_TX_BUFFER_SIZE 64
+
+#define E_NO_ERROR 0x00
+#define E_GENERAL 0x01
+#define E_BUFF_OVERFLOW 0x02
+#define E_EMPTY 0x03
 
 void serial_begin(unsigned long baud);
-uint8_t serial_read_byte();
+uint8_t serial_read_byte(uint8_t *data_out);
 uint8_t serial_available();
-void serial_write_byte(uint8_t data);
-void serial_write_chunk(void *data, uint8_t size);
+uint8_t serial_write_byte(uint8_t data);
+uint8_t serial_write_chunk(void *data, uint8_t size);
 
 
 #ifdef __cplusplus
