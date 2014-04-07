5ffb121c052fae5cdd51af7e2254df49de23f6aa
Stefan Bossbaly <sbossb@gmail.com>
2014-04-07 16:47:06 -0400

    Use while loop

--- Bartender.ino
+++ Bartender.ino
@@ -4,17 +4,28 @@
 #include "serial.h"
 #include "handler.h"
 #include "bartender.h"
+#include "timer.h"
 
 stepper_t stepper;
 handler_t handler;
 toggle_driver_t toggler;
+bartender_t bartender;
+
+// Current buffers
 uint8_t command[MSG_SIZE];
 uint8_t response[MSG_SIZE];
-bartender_t bartender;
-int size = 0;
+
+uint8_t status = 0;
+uint8_t size = 0;
+
+void handle()
+{
+	
+}
 
 void setup()
 {
+	// Move this somewhere else
 	pinMode(7, OUTPUT);
 	pinMode(8, OUTPUT);
 	
@@ -24,6 +35,7 @@ void setup()
 	// Init stepper
 	stepper_init(&stepper, 2, 3, 4, 5);
 	stepper.delay = 3;
+	stepper_release(&stepper);
 	
 	// Init Toggler
 	toggle_driver_init(&toggler, 7, 8);
@@ -33,12 +45,14 @@ void setup()
 	
 	// Init message handler
 	handler_init(&handler, &bartender);
+	
+	// Init the timer
+	timer2_init_ms(150, handle);
 }
-
 
 void loop()
 {
-	if (serial_available() > 0)
+	while (serial_available() > 0)
 	{
 		serial_read_byte(&command[size]);
 		size++;
@@ -51,5 +65,5 @@ void loop()
 		size = 0;
 	}
 	
-	delay(10);
+	delay(5);
 }
