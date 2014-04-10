74795ef7dba052920aefeaab9307fd90ab582a8d
Stefan Bossbaly <sbossb@gmail.com>
2014-04-10 14:02:20 -0400

    Change the direction of the stepper
    We are starting on the other side of the bartender now.

--- stepper.c
+++ stepper.c
@@ -21,7 +21,7 @@ void stepper_init(stepper_t *stepper, uint8_t pin0, uint8_t pin1, uint8_t pin2, 
 void stepper_step(stepper_t *stepper, uint8_t direction)
 {
 	// Calculate the next step
-	if (direction == REVERSE)
+	if (direction == FORWARD)
 	{
 		if (stepper->step == 3)
 		{
