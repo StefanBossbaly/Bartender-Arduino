8cf7ab2dd38aea2859656785163b54a5b49148b0
Stefan Bossbaly <sbossb@gmail.com>
2014-04-17 20:05:07 -0400

    Removed useless includes

--- timer.h
+++ timer.h
@@ -1,13 +1,6 @@
 #ifndef TIMER_H
 #define	TIMER_H
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include <Arduino.h>
-#else
-	#include <WProgram.h>
-	#include <pins_arduino.h>
-#endif
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
